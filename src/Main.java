cda6e69ae38faf0d714717c1e9e8fff737d314e1
Yurii <shadowjura82@gmail.com>
2023-04-12 14:16:29 +0200

    task2 has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import java.util.Arrays;
+
 public class Main {
+    public static int[] array = generateRandomArray();
+
     public static void main(String[] args) {
         task1();
         task2();
@@ -15,14 +19,20 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-        int[] array = generateRandomArray();
         int summ = 0;
         for (int x : array)
             summ += x;
         System.out.printf("Сумма трат за месяц составила %d рублей%n", summ);
     }
 
-    public static void task2(){
-
+    public static void task2() {
+        System.out.println("\nЗадача 2");
+        int maxValue = 0;
+        int minValue = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maxValue) maxValue = array[i];
+            if (array[i] < minValue) minValue = array[i];
+        }
+        System.out.printf("Минимальная сумма трат за день составила %d рублей. Максимальная сумма трат за день составила %d рублей%n", minValue, maxValue);
     }
 }
